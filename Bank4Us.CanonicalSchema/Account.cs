35a3a4407d597d62bd535ca5d4a9eb6ce3e4f157
William Leannah <william.leannah@marquette.edu>
2018-10-07 11:42:41 -0500

    Add project files.

---
+++ Bank4Us.CanonicalSchema/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank4Us.CanonicalSchema
+{
+    /// <summary>
+    ///   Course Name: MSCS 6360 Enterprise Architecture
+    ///   Year: Fall 2018
+    ///   Name: William J Leannah
+    ///   Description: Example implementation of the Domain Driven Design Pattern.
+    ///                https://en.wikipedia.org/wiki/Domain-driven_design                 
+    /// </summary>
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public DateTime OpenDate { get; set; }
+        public Decimal Balance { get; set; }
+    }
+}
---
+++ Bank4Us.CanonicalSchema/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank4Us.CanonicalSchema
+{
+    /// <summary>
+    ///   Course Name: MSCS 6360 Enterprise Architecture
+    ///   Year: Fall 2018
+    ///   Name: William J Leannah
+    ///   Description: Example implementation of the Domain Driven Design Pattern.
+    ///                https://en.wikipedia.org/wiki/Domain-driven_design                 
+    /// </summary>
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public List<Account> Accounts { get; set; }
+    }
+}
